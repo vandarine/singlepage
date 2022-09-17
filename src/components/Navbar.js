b29a7b18ad9e5a2e904b15a747f82af412f1cfc0
Schmidt Vanessa <vs095@hdm-stuttgart.de>
2022-09-18 00:06:52 +0300

    solved navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,11 @@ import {Link} from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <nav id="nav" class="navbar navbar-espand-md navbar-dark bg-dark mb-4">
+    <nav id="nav" class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
         <div class="container-fluid">
             <Link className='navbar-brand' to='/'>SPA Example</Link>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse">
-                <span class="navbar-toggle-icon"></span>
+                <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
@@ -24,5 +24,5 @@ export default function Navbar() {
             </div>
         </div>
     </nav>
-  )
+  );
 }
